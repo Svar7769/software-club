72916950d68a49b915890d80e170494bd670c872
kanuthakor <53140669+kanuthakor@users.noreply.github.com>
2021-02-17 10:53:33 +0530

    second commit

--- index.js
+++ index.js
@@ -10,19 +10,3 @@ function menuFunction() {
 }
 
 // fixed header on scroll
-
-window.onscroll = function () {
-  stickyNav();
-};
-
-var nav = document.getElementById("nav");
-
-var sticky = nav.offsetTop;
-
-function stickyNav() {
-  if (window.pageYOffset > sticky) {
-    nav.classList.add("sticky");
-  } else {
-    nav.classList.remove("sticky");
-  }
-}
